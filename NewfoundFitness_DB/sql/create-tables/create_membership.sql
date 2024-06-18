15baf8a6cb8e742de9ca8220d61fba5682e49d4e
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-06-18 04:47:31 -0230

    Delete NewfoundFitness_DB directory
    Removing directory to create as own repo.

--- NewfoundFitness_DB/sql/create-tables/create_membership.sql
+++
@@ -1,8 +0,0 @@
--- This script creates the membership table
--- The table stores information about gym memberships, linking to price
-
-CREATE TABLE membership (
-    membership_id SERIAL PRIMARY KEY,
-    price_id INT REFERENCES price(price_id),
-    membership_length INT NOT NULL
-);
--- NewfoundFitness_DB/sql/create-tables/create_staff.sql
+++
@@ -1,8 +0,0 @@
--- This script creates the staff table
--- The table stores information about staff members, linking to person_info and salary
-
-CREATE TABLE staff (
-    employee_id SERIAL PRIMARY KEY,
-    name_id INT REFERENCES person_info(name_id),
-    salary_id INT REFERENCES salary(salary_id)
-);
