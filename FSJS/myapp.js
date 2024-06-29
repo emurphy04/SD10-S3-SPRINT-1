ed1afe2ed6433451a1a6559fefa39dd8f69f1af3
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-06-29 13:12:33 +0200

    Update myapp.js

--- FSJS/myapp.js
+++ FSJS/myapp.js
@@ -1,17 +1,51 @@
 global.DEBUG = true;
 
 const fs = require('fs');
+const express = require('express');
 const myEventEmitter = require('./logEvents.js');
 
-const { initializeApplication } = require('./init.js')
-const { configApplication } = require('./config.js')
-const { tokenApplication } = require('./token.js')
+const { initializeApplication } = require('./init.js');
+const { configApplication, updateConfig, resetConfig } = require('./config.js');
+const { generateToken } = require('./token.js');
+const { updateUserRecord, searchUser } = require('./users.js');
 
 const myArgs = process.argv.slice(2);
 
-if(DEBUG) if(myArgs.length >= 1) console.log('myArgs: ', myArgs);
+if (DEBUG) if (myArgs.length >= 1) console.log('myArgs: ', myArgs);
 
-switch(myArgs[0]) {
+// Function to start the Express server
+function startServer() {
+  const app = express();
+  app.use(express.urlencoded({ extended: true }));
+
+  // Route to serve the form and handle token generation
+  app.get('/', (req, res) => {
+    res.send(`
+      <form method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <button type="submit">Submit</button>
+      </form>
+    `);
+  });
+
+  app.post('/', (req, res) => {
+    const username = req.body.username;
+    if (username) {
+      const token = generateToken(username);
+      res.send(`Your token: ${token}`);
+    } else {
+      res.send('Username is required.');
+    }
+  });
+
+  app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
+}
+
+// CLI handling code
+switch (myArgs[0]) {
   case 'init':
   case 'i':
     if (DEBUG) console.log(myArgs[0], '- init the app.');
@@ -22,10 +56,30 @@ switch(myArgs[0]) {
     if (DEBUG) console.log(myArgs[0], '- create the config folders and files.');
     configApplication();
     break;
+  case 'update-config':
+    if (DEBUG) console.log(myArgs[0], '- update config');
+    updateConfig(myArgs[1], myArgs[2]);
+    break;
+  case 'reset-config':
+    if (DEBUG) console.log(myArgs[0], '- reset config');
+    resetConfig();
+    break;
   case 'token':
   case 't':
     if (DEBUG) console.log(myArgs[0], '- manage the tokens');
-    tokenApplication();
+    generateToken(myArgs[1]);
+    break;
+  case 'update-user':
+    if (DEBUG) console.log(myArgs[0], '- update user records');
+    updateUserRecord(myArgs[1], myArgs[2], myArgs[3]);
+    break;
+  case 'search-user':
+    if (DEBUG) console.log(myArgs[0], '- search user records');
+    searchUser(myArgs[1]);
+    break;
+  case 'server':
+    if (DEBUG) console.log(myArgs[0], '- start the server');
+    startServer();
     break;
   case '--help':
   case '--h':
@@ -33,10 +87,7 @@ switch(myArgs[0]) {
     if (DEBUG) console.log(myArgs[0], '- display help');
     myEventEmitter.emit('event', process.argv, 'WARNING', 'Unrecognized CLI item requested.');
     fs.readFile(__dirname + "/usage.txt", (error, data) => {
-      if(error) throw error;
+      if (error) throw error;
       console.log(data.toString());
     });
 }
-
-
-
