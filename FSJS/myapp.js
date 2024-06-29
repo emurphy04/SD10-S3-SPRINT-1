9b4a39ffe23a3e9ff6765e2850c9a70f79409095
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-06-29 13:57:43 +0200

    Updating CLI

--- FSJS/myapp.js
+++ FSJS/myapp.js
@@ -6,7 +6,7 @@ const myEventEmitter = require('./logEvents.js');
 
 const { initializeApplication } = require('./init.js');
 const { configApplication, updateConfig, resetConfig } = require('./config.js');
-const { generateToken } = require('./token.js');
+const { generateToken, listTokens, countTokens } = require('./token.js');
 const { updateUserRecord, searchUser } = require('./users.js');
 
 const myArgs = process.argv.slice(2);
@@ -43,6 +43,36 @@ function startServer() {
   });
 }
 
+function displayHelp() {
+  console.log(`
+    myapp <command> <option>
+
+    Usage:
+
+    myapp --help          displays help
+
+    myapp init            initialize the app
+    myapp init --mk       create all the app folders
+    myapp init --cat      create all the app files
+    myapp init --all      create all the folders and files
+
+    myapp config          manage configuration
+    myapp config --show   show the contents of the config file
+    myapp config --reset  reset to default the config file
+    myapp config --set <key> <value>    set a specific attribute of the config file
+
+    myapp token           manage user tokens
+    myapp token --list    list all the tokens
+    myapp token --count   provide a count of all the tokens
+    myapp token --new <username>     generate a new token for the user
+
+    myapp update-user <username> <email> <phone>   update user records
+    myapp search-user <query>   search user records by username, email, or phone number
+
+    myapp server          start the web server for token generation
+  `);
+}
+
 // CLI handling code
 switch (myArgs[0]) {
   case 'init':
@@ -52,29 +82,58 @@ switch (myArgs[0]) {
     break;
   case 'config':
   case 'c':
-    if (DEBUG) console.log(myArgs[0], '- create the config folders and files.');
-    configApplication();
-    break;
-  case 'update-config':
-    if (DEBUG) console.log(myArgs[0], '- update config');
-    updateConfig(myArgs[1], myArgs[2]);
-    break;
-  case 'reset-config':
-    if (DEBUG) console.log(myArgs[0], '- reset config');
-    resetConfig();
+    if (DEBUG) console.log(myArgs[0], '- manage configuration');
+    if (myArgs.length < 2) {
+      displayHelp();
+    } else if (myArgs[1] === '--show') {
+      configApplication();
+    } else if (myArgs[1] === '--set') {
+      if (myArgs.length < 4) {
+        console.log('Usage: myapp config --set <key> <value>');
+      } else {
+        updateConfig(myArgs[2], myArgs[3]);
+      }
+    } else if (myArgs[1] === '--reset') {
+      resetConfig();
+    } else {
+      displayHelp();
+    }
     break;
   case 'token':
   case 't':
     if (DEBUG) console.log(myArgs[0], '- manage the tokens');
-    generateToken(myArgs[1]);
+    if (myArgs.length < 2) {
+      displayHelp();
+    } else if (myArgs[1] === '--new') {
+      if (myArgs.length < 3) {
+        console.log('Usage: myapp token --new <username>');
+      } else {
+        const token = generateToken(myArgs[2]);
+        console.log(`Generated token for ${myArgs[2]}: ${token}`);
+      }
+    } else if (myArgs[1] === '--list') {
+      listTokens();
+    } else if (myArgs[1] === '--count') {
+      countTokens();
+    } else {
+      displayHelp();
+    }
     break;
   case 'update-user':
     if (DEBUG) console.log(myArgs[0], '- update user records');
-    updateUserRecord(myArgs[1], myArgs[2], myArgs[3]);
+    if (myArgs.length < 4) {
+      console.log('Usage: myapp update-user <username> <email> <phone>');
+    } else {
+      updateUserRecord(myArgs[1], myArgs[2], myArgs[3]);
+    }
     break;
   case 'search-user':
     if (DEBUG) console.log(myArgs[0], '- search user records');
-    searchUser(myArgs[1]);
+    if (myArgs.length < 2) {
+      console.log('Usage: myapp search-user <query>');
+    } else {
+      searchUser(myArgs[1]);
+    }
     break;
   case 'server':
     if (DEBUG) console.log(myArgs[0], '- start the server');
@@ -83,10 +142,7 @@ switch (myArgs[0]) {
   case '--help':
   case '--h':
   default:
-    if (DEBUG) console.log(myArgs[0], '- display help');
+    displayHelp();
     myEventEmitter.emit('event', process.argv, 'WARNING', 'Unrecognized CLI item requested.');
-    fs.readFile(__dirname + "/usage.txt", (error, data) => {
-      if (error) throw error;
-      console.log(data.toString());
-    });
 }
+
