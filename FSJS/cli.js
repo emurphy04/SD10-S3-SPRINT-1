4f7b91b8ef89fc3a0d6fc9db5cfb78d1e4ec1a59
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-06-29 11:25:17 +0200

    Add view configuration functionality

--- FSJS/cli.js
+++ FSJS/cli.js
@@ -32,22 +32,6 @@ function viewConfig() {
   }
 }
 
-function updateConfig(key, value) {
-  if (fs.existsSync(path)) {
-    const config = JSON.parse(fs.readFileSync(path, 'utf-8'));
-    config[key] = value;
-    fs.writeFileSync(path, JSON.stringify(config, null, 2));
-    console.log(`Updated ${key} to ${value}.`);
-  } else {
-    console.log('Configuration file is missing.');
-  }
-}
-
-function resetConfig() {
-  fs.writeFileSync(path, JSON.stringify(defaultConfig, null, 2));
-  console.log('Configuration reset to default.');
-}
-
 // Command-line interface handling
 const command = process.argv[2];
 if (command === 'init') {
@@ -56,10 +40,4 @@ if (command === 'init') {
   status();
 } else if (command === 'view-config') {
   viewConfig();
-} else if (command === 'update-config') {
-  const key = process.argv[3];
-  const value = process.argv[4];
-  updateConfig(key, value);
-} else if (command === 'reset-config') {
-  resetConfig();
 }
