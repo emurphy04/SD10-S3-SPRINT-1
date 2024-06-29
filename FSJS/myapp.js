c40073d9208f73447002234e6989f80f80ee8119
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-06-29 12:49:24 +0200

    Add web form for end user token generation

--- FSJS/myapp.js
+++ FSJS/myapp.js
@@ -1,17 +1,49 @@
 global.DEBUG = true;
 
 const fs = require('fs');
+const express = require('express');
+const crypto = require('crypto');
 const myEventEmitter = require('./logEvents.js');
 
-const { initializeApplication } = require('./init.js')
-const { configApplication } = require('./config.js')
-const { tokenApplication } = require('./token.js')
+const { initializeApplication } = require('./init.js');
+const { configApplication } = require('./config.js');
+const { tokenApplication } = require('./token.js');
 
 const myArgs = process.argv.slice(2);
 
-if(DEBUG) if(myArgs.length >= 1) console.log('myArgs: ', myArgs);
+if (DEBUG) if (myArgs.length >= 1) console.log('myArgs: ', myArgs);
 
-switch(myArgs[0]) {
+// Initialize Express app
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+// Route to serve the form and handle token generation
+app.get('/', (req, res) => {
+  res.send(`
+    <form method="post">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required>
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  const username = req.body.username;
+  if (username) {
+    const token = crypto.createHash('sha256').update(username).digest('hex');
+    res.send(`Your token: ${token}`);
+  } else {
+    res.send('Username is required.');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+// Existing CLI handling code
+switch (myArgs[0]) {
   case 'init':
   case 'i':
     if (DEBUG) console.log(myArgs[0], '- init the app.');
@@ -33,7 +65,7 @@ switch(myArgs[0]) {
     if (DEBUG) console.log(myArgs[0], '- display help');
     myEventEmitter.emit('event', process.argv, 'WARNING', 'Unrecognized CLI item requested.');
     fs.readFile(__dirname + "/usage.txt", (error, data) => {
-      if(error) throw error;
+      if (error) throw error;
       console.log(data.toString());
     });
 }
